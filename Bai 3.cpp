a1f967f17243764283db3606d273771c534ac1b9
Blackhacker-a <64885014+Blackhacker-a@users.noreply.github.com>
2020-05-19 16:29:35 +0700

    e fix btap 3

--- Bai 3.cpp
+++ Bai 3.cpp
@@ -2,50 +2,23 @@
 int main(){
 	int n;
 	do{
-	printf("Nhap so phan tu cua mang \n");
-	scanf("%d",&n);
+		printf("Nhap so phan tu cua mang ");
+		scanf("%d",&n);
 	} while(n<2);
 	int arr[n];
 	for(int i=0;i<n;i++){
-		printf("Nhap phan tu thu %d cua mang \n",i);
+		printf("Nhap phan tu thu %d cua mang ",i);
 		scanf("%d",&arr[i]);
 	}
-	
-	int count;
-//	int x=arr[0];
-	for(int i=1;i<n;i++){
-		if(arr[i]!=arr[0]){
-		   count=0;
+	for(int j=0;j<n;j++){
+	for(int i=j+1;i<n;i++){
+		if(arr[j]==arr[i]){
+			printf("Nhap lai phan tu thu %d cua mang ",i);
+		scanf("%d",&arr[i]);
 		}
-		if(arr[i]==arr[0]){
-			count=1;
-		}
-	}
-	if(count==0){
-	   printf("Cac so ban nhap cua mang phan biet ");
-	}
-	else if(count==1){
-	do{
-		for(int i=0;i<n;i++){
-			printf("Nhap lai phan tu thu %d cua mang \n",i);
-		    scanf("%d",&arr[i]);
-		}
-		for(int i=0;i<n;i++){    
-		    if(arr[i]!=arr[0]){
-		    	count=0;
-		    }
-		    if(arr[i]==arr[0]){
-		    	count=1;
-		    }
-		}    	
-	}while(count==1);
-	if(count==0){
-		printf("Cac so ban nhap cua mang phan biet ");
 	}
     }
-}		
-
-	
+}
 	
 	
 	
